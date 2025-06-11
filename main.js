257d263302644d130dd0637eb41c572ab115ed61
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-06-11 10:13:33 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("register-btn").addEventListener("click", function () {
+    window.location.href = "/html/register.html";
+  });
+  document.getElementById("login-btn").addEventListener("click", function () {
+    window.location.href = "/html/login.html";
+  });
+  document.getElementById("learn more-btn").addEventListener("click", function () {
+    window.location.href = "/html/learn-more.html";
+  });
+
+document.getElementById("subscribe-btn").addEventListener("click", function () {
+    window.location.href = "/html/login.html";
+  });
+
+    
